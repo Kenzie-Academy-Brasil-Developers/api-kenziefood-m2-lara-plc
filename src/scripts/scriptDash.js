fb976126fe6268fe1d4ed560f000f6fe94ef464b
Vitória Cisne <anavitoriacisn314@gmail.com>
2022-05-18 09:50:05 -0300

    interaction-working

--- src/scripts/scriptDash.js
+++ src/scripts/scriptDash.js
@@ -1,8 +1,11 @@
 import { TemplateDashProducts } from "../models/TemplateDashProducts.js";
 
+localStorage.getItem('token') ?
+null :
+window.location.href = "/index.html"
+
+
 TemplateDashProducts.getMyProducts()
-
-
 
 document.querySelector('.addProduct').addEventListener('click', function() {
     TemplateDashProducts.showModal()
