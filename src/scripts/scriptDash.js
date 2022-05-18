e84378e6f2b4c87d0966628bc2003e94bda448f6
Vitória Cisne <anavitoriacisn314@gmail.com>
2022-05-18 12:29:03 -0300

    header-dashboard

--- src/scripts/scriptDash.js
+++ src/scripts/scriptDash.js
@@ -5,11 +5,37 @@ null :
 window.location.href = "/index.html"
 
 
-TemplateDashProducts.getMyProducts()
+TemplateDashProducts.getMyProductsFilter('Todos')
+
+if(localStorage.getItem('user')) {
+    document.querySelector('#nameUser').innerText = localStorage.getItem('user')
+}
 
 document.querySelector('.addProduct').addEventListener('click', function() {
     TemplateDashProducts.showModal()
     
 })
 
+const drop = document.querySelector('.dropdown img') 
+drop.addEventListener('click', () => {
+    drop.classList.toggle('show-drop')
+})
 
+
+const filters = document.querySelectorAll('.filter div')
+
+filters.forEach((filter) => {
+    filter.addEventListener('click', () => {
+        filters.forEach((filter) => filter.classList.remove('active-filter'))
+        filter.classList.add('active-filter')
+
+        TemplateDashProducts.getMyProductsFilter(`${filter.innerText}`)
+        
+    })
+
+
+
+})
+
+const search = document.querySelector('#search-input')
+search.addEventListener('input', () =>  TemplateDashProducts.getMyProductsSearch(search.value))
